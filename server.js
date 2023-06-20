a10cc7fcb8c4093e21e61596815bfa21804e0c64
Vincent Phung <vincent.j.phung@gmail.com>
2023-06-19 21:17:32 -0700

    Refactor server routes for better handling of static files

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ app.use(cors());
 
 const apiKey = process.env.API_KEY; 
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle the requests for the individual files like main.js, main.css, etc.
+app.get(/^\/(static|favicon\.ico|manifest\.json|logo192\.png)/, (req, res) => {
+  const filePath = path.join(__dirname, 'build', req.url);
+  res.sendFile(filePath);
+});
 
 app.get('/search', (req, res) => {
   axios({
@@ -26,8 +33,9 @@ app.get('/search', (req, res) => {
     });
 });
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
