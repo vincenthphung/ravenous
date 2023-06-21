6aebba1f953e09d232f735e5e7cb2f35615dcdbf
Vincent Phung <vincent.j.phung@gmail.com>
2023-06-20 20:21:26 -0700

    Removed build directory from version control

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -18,7 +18,17 @@ const Yelp = {
 
         if (jsonResponse.businesses) {
           return jsonResponse.businesses.map((business) => ({
-            // ... (rest of your code)
+ id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+            url: business.url,
           }));
         } else {
           return [];
